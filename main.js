31fc6ffaebcacdad3834c3f86f7e8cf8baa3dd60
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-04-07 12:48:17 -0400

    Adds Html and JS to card title and button

---
+++ main.js
@@ -0,0 +1,7 @@
+const URL = ""
+// Query change dog image button
+const changeDogButton = document.querySelector("#change-dog-button");
+//add eventListener to dogbutton
+changeDogButton.addEventListener('click', function(){
+    console.log("button was clicked");
+})
