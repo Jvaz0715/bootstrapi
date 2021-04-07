bd1103d845da3f33aa566b49425449b156b7cd65
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-04-07 15:24:57 -0400

    Starts to solve event listener for weather button, creates functionality to change url based on city searched

--- main.js
+++ main.js
@@ -34,6 +34,29 @@ changeDogButton.addEventListener('click', function(){
 const checkWeatherButton = document.querySelector("#weather-button");
 
 // test button
-checkWeatherButton.addEventListener('click', function() {
-    console.log("weather button clicked")
+checkWeatherButton.addEventListener('click', function(){
+    console.log("button was clicked");
+    //API Request Below
+    // =================
+    //query the city input
+    const cityInput = document.querySelector("#city-input");
+    //get the city input converted to lowercase string and no spaces to append later on url for search
+    const citySearched = cityInput.value.toLowerCase().replace(/ /g, '');
+    //console.log(citySearched)
+    // This is the weather api URL
+    // This also  will append the city searched to the end of the url in order to get the city
+    const URL = "https://goweather.herokuapp.com/weather/" + citySearched;
+    console.log(URL)
+    //make a request to the URL
+    /*fetch(URL)
+        // Now process the raw response into an object
+        .then((res) => res.json())
+        // Now process the above JSON, we will first console log a message
+        //but we want to use this space to change the img src
+        .then((data) => {
+            console.log('success!');
+            //const dogImage = document.querySelector("#card-img-top");
+            //console.log(data.message);
+            dogImage.src = data.message;
+        })*/
 })
