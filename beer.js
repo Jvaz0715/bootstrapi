6ac7e78727a4a8adf657234910ced98459891d03
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-10 07:36:51 -0400

    Queries the beer button

--- beer.js
+++ beer.js
@@ -1,3 +1,12 @@
 // FYK: the beers urls go up to 325 beers
 // https://api.punkapi.com/v2/beers/1 - https://api.punkapi.com/v2/beers/325
 
+//========
+//========Beer App section========
+
+//query the beer randomizer button
+const getBeerButton = document.querySelector("#beer-button");
+//test event listener to make sure click works
+getBeerButton.addEventListener('click', function(){
+    console.log("Beer button clicked");
+})
