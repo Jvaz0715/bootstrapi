f96ba98eebb2f51bcea26190c054ca8eb56865b5
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-10 09:00:28 -0400

    begins wiring up each section of the beer display

--- beer.js
+++ beer.js
@@ -24,8 +24,21 @@ getBeerButton.addEventListener('click', function(){
         //but we want to use this space to change the img src
         .then((data) => {
             console.log('success!');
-            console.log(data[0].id)
+            console.log(data[0].first_brewed)
+            console.log(typeof data[0].abv)
             console.log(data[0].name)
+            console.log(data[0].id)
+            console.log(data[0].description)
+
+            //query all the spots you need to update on click
+            const ABV = document.querySelector(".abv");
+            ABV.innerText = `${data[0].abv}%`;
+            const dateBrewed = document.querySelector(".brewdate");
+            const beerImg = document.querySelector(".beer-img");
+            const beerName = document.querySelector("#beer-name");
+            const beerTag = document.querySelector("#beer-tagline")
+            const beerDescription = document.querySelector(".beer-description");
+    
 
         })
 
