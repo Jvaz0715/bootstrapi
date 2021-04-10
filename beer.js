598b559f44100257f0146947bee295ee61d56741
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-10 08:40:19 -0400

    Targets data

--- beer.js
+++ beer.js
@@ -6,7 +6,27 @@
 
 //query the beer randomizer button
 const getBeerButton = document.querySelector("#beer-button");
-//test event listener to make sure click works
+
+//build out button event
 getBeerButton.addEventListener('click', function(){
+    //test event listener to make sure click works
     console.log("Beer button clicked");
+    //API Request Below
+    // =================
+    // This is the random beer api URL
+    const URL = "https://api.punkapi.com/v2/beers/random";
+
+    //make a request to the URL
+    fetch(URL)
+        // Now process the raw response into an object
+        .then((res) => res.json())
+        // Now process the above JSON, we will first console log a message
+        //but we want to use this space to change the img src
+        .then((data) => {
+            console.log('success!');
+            console.log(data[0].id)
+            console.log(data[0].name)
+
+        })
+
 })
